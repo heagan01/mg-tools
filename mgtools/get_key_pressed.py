6ab1783ea880e6758768dac86d093e4eea4050a3
Meir Gabay <unfor19@gmail.com>
2019-09-02 12:56:15 +0300

    avoided global variable

--- mgtools/get_key_pressed.py
+++ mgtools/get_key_pressed.py
@@ -1,13 +1,12 @@
 from tkinter import Tk, Frame
 
 
-def __set_key(e, root):
+def __set_key(e, root, key_pressed):
     """
     e - event with attribute 'char', the released key
     """
-    global key_pressed
     if e.char:
-        key_pressed = e.char
+        key_pressed['value'] = e.char
         root.destroy()
 
 
@@ -16,27 +15,30 @@ def get_key(msg="Press any key ...", time_to_sleep=3):
     msg - set to empty string if you don't want to print anything
     time_to_sleep - default 3 seconds
     """
-    global key_pressed
     if msg:
         print(msg)
-    key_pressed = None
+    key_pressed = {"value": ''}
     root = Tk()
     root.overrideredirect(True)
     frame = Frame(root, width=0, height=0)
-    frame.bind("<KeyRelease>", lambda f: __set_key(f, root))
+    frame.bind("<KeyRelease>", lambda f: __set_key(f, root, key_pressed))
     frame.pack()
     root.focus_set()
     frame.focus_set()
     frame.focus_force()  # doesn't work in a while loop without it
     root.after(time_to_sleep * 1000, func=root.destroy)
-    root.mainloop()
+    try:
+        root.mainloop()
+    except KeyboardInterrupt as e:
+            root.destroy()
+            key_pressed['value'] = None
     root = None  # just in case
-    return key_pressed
+    return key_pressed['value']
 
 
 def __main():
-        c = None
-        while not c:
+        c = ''
+        while c == '':
                 c = get_key("Choose your weapon ... ", 2)
         print(c)
 
